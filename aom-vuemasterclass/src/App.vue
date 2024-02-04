a85358d5c2cabc41bfb53f4488da8e6843500062
thamakorn.k <thamakorn.aom31@gmail.com>
2024-02-04 21:11:00 +0700

    feat: test for write form data

--- aom-vuemasterclass/src/App.vue
+++ aom-vuemasterclass/src/App.vue
@@ -1,30 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div>
+      <div>Fistname</div>
+      <input type="text" name="firstname" v-model="formData.firstname" />
+    </div>
+    <div>
+      <div>Lastname</div>
+      <input type="text" name="lastname" v-model="formData.lastname"/>
+    </div>
+    <div>
+      <div>Description</div>
+      <textarea name="description" cols="30" rows="10" v-model="formData.description"></textarea>
+    </div>
+    <div>
+      Gender
+      <div v-for="gender in genderList">
+        <input type="radio" name="gender" :value="gender" v-model="formData.gender" />
+        {{ gender }}
+      </div>
+    </div>
+
+    <div>
+      Interest
+      <div v-for="interest in interestList">
+        <input type="checkbox" name="interest" :value="interest" v-model="formData.interest"/>
+        {{ interest }}
+      </div>
+    </div>
+
+    <div>
+      <button @click="sendForm()">Send</button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import { reactive } from "vue";
+import { FormDataInput } from "./models/form";
+
+const genderList= ["men", "women", "none"];
+const interestList = ["book", "sports", "movie", "travel"];
+
+const formData = reactive<FormDataInput>({
+  firstname: "",
+  lastname: "",
+  description: "",
+  gender: "",
+  interest: [],
+});
+
+function sendForm() {
+  console.log(formData)
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style>
+/* Your styles here */
 </style>
---
+++ aom-vuemasterclass/src/models/form.ts
@@ -0,0 +1,7 @@
+export interface FormDataInput {
+    firstname: string
+    lastname: string
+    gender: string
+    interest: string[]
+    description: string
+}
